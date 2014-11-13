1bc788d5c07fdad04cba3e99dffe797d98857959
Wladimir Sidorenko <wlsidorenko@gmail.com>
2014-11-13 19:01:14 +0100

    bug #28 fixed (unlink-node deleted a text node)

--- test/all.tcl
+++ test/all.tcl
@@ -9,16 +9,19 @@ global DIRNAME
 # Main
 
 # Run all .test files in this file's directory.
-namespace eval :: source "$DIRNAME/RSTTool.tcl"
 
-foreach t [lsort -dict [glob -directory [file dirname [file normalize [info script]]] *.test.tcl]] {
-    puts -nonewline stderr "$t ..."
-    if {[catch {
-	namespace eval :: source $t
-    }]} {
-	puts stderr " FAILED"
-	puts stderr $::errorInfo
-    } else {
-	puts stderr " SUCCESS"
+if {[catch {namespace eval :: source "$DIRNAME/RSTTool.tcl"}]} {
+    puts stderr "Failed to load the program..."
+} else {
+    foreach t [lsort -dict [glob -directory [file dirname [file normalize [info script]]] *.test.tcl]] {
+	puts -nonewline stderr "$t ..."
+	if {[catch {
+	    namespace eval :: source $t
+	}]} {
+	    puts stderr " FAILED"
+	    puts stderr $::errorInfo
+	} else {
+	    puts stderr " SUCCESS"
+	}
     }
 }
