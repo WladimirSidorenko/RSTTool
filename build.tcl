c4f63518099c28c33478b9621c7a71af917a417b
Wladimir Sidorenko <wlsidorenko@gmail.com>
2015-04-28 16:18:42 +0200

    obsolete files removed, build.tcl updated, tclkit added

--- build.tcl
+++ build.tcl
@@ -58,6 +58,47 @@ proc _test {} {
     return
 }
 
+proc Hstarkit {} { return "?destination? ?interpreter?\n\tGenerate a starkit\n\tdestination = path of result file, default 'critcl.kit'\n\tinterpreter = (path) name of tcl shell to use for execution, default 'rsttool'" }
+proc _starkit {{dst rsttool.kit} {interp tclkit}} {
+    package require vfs::mk4;
+
+    set c [open $dst w];
+    fconfigure $c -translation binary -encoding binary;
+    puts -nonewline $c "#!/bin/sh\n# -*- tcl -*- \\\nexec $interp \"\$0\" \$\{1+\"\$@\"\}\npackage require starkit\nstarkit::header mk4 -readonly\n\032################################################################################################################################################################";
+    close $c;
+
+    vfs::mk4::Mount $dst /KIT;
+    file copy -force lib /KIT;
+    file copy -force main.tcl /KIT;
+    vfs::unmount /KIT;
+    +x $dst;
+
+    puts stderr "Created starkit: $dst";
+    return;
+}
+
+proc Hstarpack {} { return "prefix ?destination?\n\tGenerate a fully-selfcontained executable, i.e. a starpack\n\tprefix      = path of tclkit/basekit runtime to use\n\tdestination = path of result file, default 'rsttool'" }
+proc _starpack {prefix {dst rsttool}} {
+    package require vfs::mk4;
+
+    file copy -force $prefix $dst;
+
+    vfs::mk4::Mount $dst /KIT;
+    file mkdir /KIT/lib;
+
+    foreach d [glob -directory lib *] {
+	file delete -force  /KIT/lib/[file tail $d];
+	file copy -force $d /KIT/lib;
+    }
+
+    file copy -force main.tcl /KIT;
+    vfs::unmount /KIT;
+    +x $dst;
+
+    puts stderr "Created starpack: $dst";
+    return;
+}
+
 ##################################################################
 # Main
 main
